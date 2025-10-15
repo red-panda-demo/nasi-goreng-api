7d55bcc013045ef656c4ed8891bd7cf5e8dad173
TINGWEIJING <tingweijingting2000@gmail.com>
2025-10-15 16:29:43 +0800

    first commit

---
+++ src/main/java/com/example/nasigorengapi/NasigorengapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasigorengapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasigorengapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasigorengapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nasigorengapi/NasigorengapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasigorengapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasigorengapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
